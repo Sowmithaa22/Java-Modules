9429a18bf1f365a5a17073809de1595fb9a1c12d
Sowmithaa Sri S <137026975+Sowmithaa22@users.noreply.github.com>
2026-01-04 15:32:58 +0530

    Implement pyramid pattern printing in Java
    Added a program to print a pyramid pattern based on user input.

--- pyramid.java
+++ pyramid.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
 
+public class main {
+  public static void main (String[] args) {
+    Scanner sc= new Scanner(System.in);
+    System.out.println("Enter the number of rows:");
+    int rows= sc.nextInt();
+    
+    for (int i=1; i<rows; i++) {
+      for (int space=1; space<=rows-i; space++) {
+        System.out.print(" ");
+      }
+      for (int j=1; j <= (2*i-1); j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+}
