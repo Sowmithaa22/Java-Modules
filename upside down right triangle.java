c11da14b99e7806d2844ee1a4691a1cbf00ed921
Sowmithaa Sri S <137026975+Sowmithaa22@users.noreply.github.com>
2026-01-08 14:00:16 +0530

    Implement second approach for upside down triangle
    Added a second approach to print an upside down right triangle.

--- upside down right triangle.java
+++ upside down right triangle.java
@@ -1,7 +1,22 @@
 class Main {
+   //Approach 1:
     public static void main(String[] args) {
         for(int i=0;i<=10; i++) {
             for (int j=i; j<=10; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// Approach 2:
+
+
+class Main {
+    public static void main(String[] args) {
+        for(int i=10;i>=0; i--) {
+            for (int j=0; j<=i; j++) {
                 System.out.print(" * ");
             }
             System.out.println();
